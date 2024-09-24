ea8ebafcd702536ba292644d9451ac839bd7ffda
EliBarton <70331034+EliBarton@users.noreply.github.com>
2024-09-24 16:19:23 -0600

    deleted all the beef

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const app = express();
-const DB = require('./database.js');
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const { webSocket } = require('./websocket.js');
-
-const authCookieName = 'yourlogin';
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -13,8 +7,6 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
-// Use the cookie parser middleware for tracking authentication tokens
-app.use(cookieParser());
 
 // Header necessary to run the galaga game
 app.use((req, res, next) => {
@@ -29,125 +21,7 @@ app.use(express.static('public'));
 app.set('trust proxy', true)
 
 
-// Router for service endpoints
-var apiRouter = express.Router();
-app.use(`/api`, apiRouter);
-
-// GetScores
-apiRouter.get('/scores', async (_req, res) => {
-  const scores = await DB.getAllScores();
-  res.send(scores);
-});
-
-// SubmitScore
-apiRouter.post('/score', async (req, res) => {
-  await DB.updateScore(req.body)
-  const scores = await DB.getAllScores();
-  res.send(scores);
-});
-
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
-
-
-// CreateAuth token for a new user
-apiRouter.post('/auth/create', async (req, res) => {
-  if (await DB.getUser(req.body.name)) {
-    res.status(409).send({ msg: 'Existing user' });
-  } else {
-    const user = await DB.createUser(req.body.name, req.body.password);
-
-    // Set the cookie
-    setAuthCookie(res, user.token);
-
-    res.send({
-      id: user._id,
-    });
-  }
-});
-
-// GetAuth token for the provided credentials
-apiRouter.post('/auth/login', async (req, res) => {
-  const user = await DB.getUser(req.body.name);
-  if (user) {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      setAuthCookie(res, user.token);
-      res.send({ id: user._id });
-      return;
-    }
-  }
-  res.status(401).send({ msg: 'Unauthorized' });
-});
-
-
-// secureApiRouter verifies credentials for endpoints
-var secureApiRouter = express.Router();
-apiRouter.use(secureApiRouter);
-
-secureApiRouter.use(async (req, res, next) => {
-  authToken = req.cookies[authCookieName];
-  const user = await DB.getUserByToken(authToken);
-  if (user) {
-    next();
-  } else {
-    res.status(401).send({ msg: 'Unauthorized' });
-  }
-});
-
-// GetScores
-secureApiRouter.get('/scores', async (req, res) => {
-  const scores = await DB.getHighScores();
-  res.send(scores);
-});
-
-// SubmitScore
-secureApiRouter.post('/score', async (req, res) => {
-  const score = { ...req.body, ip: req.ip };
-  await DB.addScore(score);
-  const scores = await DB.getHighScores();
-  res.send(scores);
-});
-
-// Default error handler
-app.use(function (err, req, res, next) {
-  res.status(500).send({ type: err.name, message: err.message });
-});
-
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
-
-// setAuthCookie in the HTTP response
-function setAuthCookie(res, authToken) {
-  res.cookie(authCookieName, authToken, {
-    secure: true,
-    httpOnly: true,
-    sameSite: 'strict',
-  });
-}
-
-// DeleteAuth token if stored in cookie
-apiRouter.delete('/auth/logout', (_req, res) => {
-  res.clearCookie(authCookieName);
-  res.status(204).end();
-});
-
-// GetUser returns information about a user
-apiRouter.get('/user/:name', async (req, res) => {
-  const user = await DB.getUser(req.params.name);
-  if (user) {
-    const token = req?.cookies.token;
-    res.send({ name: user.name, authenticated: token === user.token });
-    return;
-  }
-  res.status(404).send({ msg: 'Unknown' });
-});
-
-const httpService = app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-webSocket(httpService);
