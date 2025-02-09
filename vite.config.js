a0a1b4a7ea48c5c360fecae9845c7a8b2728cf11
EliBarton <70331034+EliBarton@users.noreply.github.com>
2025-02-09 10:06:21 -0700

    background image route fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/FozzGames/', // Replace with your actual GitHub repo name
   build: {
     outDir: 'docs'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'FozzGames')
+    }
   }
 });
