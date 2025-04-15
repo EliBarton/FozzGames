023815dfbf4b35f65f9ea703cd4cbee2ebe79110
elidbarton <elidbarton@gmail.com>
2025-04-15 14:48:02 +0000

    update build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/FozzGames/', // Replace with your actual GitHub repo name
   build: {
-    outDir: 'docs'
+    outDir: 'dist'
   },
   resolve: {
     alias: {
