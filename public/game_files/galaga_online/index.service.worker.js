fc31f83042af6a0f19b2ab1d4ec5b8417be64141
EliBarton <70331034+EliBarton@users.noreply.github.com>
2025-06-16 14:22:04 -0600

    add debug

--- public/game_files/galaga_online/index.service.worker.js
+++ public/game_files/galaga_online/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1750104856|2395375417";
+const CACHE_VERSION = "1750105284|2823256499";
 const CACHE_PREFIX = "GalagaOnline-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
