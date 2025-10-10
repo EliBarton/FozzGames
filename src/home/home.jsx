34f244098a24f4ecc922fdbf04c9743a5854738d
elidbarton <elidbarton@gmail.com>
2025-10-10 22:48:05 +0000

    fix home screen

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -10,31 +10,36 @@ const HomePage = () => {
             <p>Featured game:</p>
             <div className="banner">
                 <div className="banner-overlay">
-                    <p className="Welcome">Lord of the Donuts
+                    <p className="Welcome">Loopy Dogfights
                     </p>
-                    <Link type="button" className="btn btn-primary btn-lg" to='games/lordofdonuts'>Play</Link>
-                    <p>Todd must save his donuts, and the world.</p>
+                    <Link type="button" className="btn btn-primary btn-lg" to='games/loopydogfights'>Play</Link>
+                    <p>Experience aerial combat with a twist!</p>
                 </div>
             </div>
 
             <p>Latest:</p>
 
-            <div className="item-list">
-                <li>
-                    <Link className="list-item" to="games/dogbubbles">
-                        <img src={`/imgs/dogbubbles.png`} alt="Dog Bubbles" height="100" />
-                        Dog Bubbles</Link>
-                </li>
-                <li>
-                    <Link className="list-item" to="games/lordofdonuts">
-                        <img src={`/imgs/lordofdonuts.png`} alt="Lord of the Donuts" height="100" />
-                        Lord of the Donuts</Link>
-                </li>
-                <li>
-                    <Link className="list-item" to="games/galaga">
-                        <img src={`/imgs/galaga.png`} alt="Galaga Online" height="100" />
-                        Galaga Online</Link>
-                </li>
+            <div className="latest-games-panel">
+                <div className="item-list">
+                    <li>
+                        <Link className="list-item" to="games/loopydogfights">
+                            <img src={`/imgs/loopydogfights.png`} alt="Loopy Dogfights" height="100" />
+                            <span>Loopy Dogfights</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className="list-item" to="games/dogbubbles">
+                            <img src={`/imgs/dogbubbles.png`} alt="Dog Bubbles" height="100" />
+                            <span>Dog Bubbles</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className="list-item" to="games/lordofdonuts">
+                            <img src={`/imgs/lordofdonuts.png`} alt="Lord of the Donuts" height="100" />
+                            <span>Lord of the Donuts</span>
+                        </Link>
+                    </li>
+                </div>
             </div>
         </div>
     );
